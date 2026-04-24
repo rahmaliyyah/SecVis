88939e1c219bf4681b537b8fea653fe123a1b103
Rahma <rahmaaliyyah29@gmail.com>
2026-04-25 06:38:25 +0700

    Added vite react component

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  withCredentials: true,
+})
+
+// Auto attach token ke setiap request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// Auto redirect ke login kalau token expired
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
